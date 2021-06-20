c0b4b7724d9f5fa9ce381220e46e7fe11b10dfef
Muhammadsyafii29 <syafii.01.muhammad@gmail.com>
2021-06-20 12:56:41 +0800

    membuat object dari class1 dan class2

--- src/model/AksiLaptop.java
+++ src/model/AksiLaptop.java
@@ -1,5 +1,34 @@
 package model;
 
 public class AksiLaptop {
-    public static void main(String[])
+    public static void main(String[] args){
+
+        //Object Class 1
+        Laptop laptop = new Laptop();
+        laptop.namaLaptop = "Asus";
+        laptop.jenisLaptop = "Asus Ultraslim";
+        laptop.modelLaptop = "X441B";
+        laptop.buatanLaptop = "China";
+        laptop.warnaLaptop = "Hitam";
+        laptop.jenisInpedia = "Nvidia GeForce GTX 1080 Ti";
+        laptop.deskripsiLaptop = "Anti Air";
+        laptop.Compactdisk = "CD";
+        laptop.tampilkanAtribut();
+        laptop.create();
+        laptop.create("Asus Vivobook");
+
+        //Object Class 2
+        pembeliLaptop pembeli = new pembeliLaptop();
+        pembeli.namaLaptop = "Lenovo";
+        pembeli.jenisLaptop = "Lenovo Ideapad";
+        pembeli.modelLaptop = "330 14";
+        pembeli.buatanLaptop = "China";
+        pembeli.warnaLaptop = "Biru";
+        pembeli.jenisInpedia = "Legion Y520 6ID";
+        pembeli.deskripsiLaptop = "Anti Air";
+        pembeli.Compactdisk = "CD";
+        pembeli.tampilkanAtribut();
+        pembeli.create();
+        pembeli.create("Lenovo Yoga");
+    }
 }
